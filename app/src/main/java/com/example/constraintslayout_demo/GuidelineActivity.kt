ac1e483b7ec3fc956114250e430f51fe569bc90a
Egidjus Gecius <e.gecius@gmail.com>
2019-08-19 21:19:58 +0100

    rename to GuidelineActivity

--- app/src/main/java/com/example/constraintslayout_demo/GuidelineActivity.kt
+++ app/src/main/java/com/example/constraintslayout_demo/GuidelineActivity.kt
@@ -3,10 +3,11 @@ package com.example.constraintslayout_demo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+/** Shows how to use guidelines in ConstraintsLayout */
+class GuidelineActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_guideline)
     }
 }
