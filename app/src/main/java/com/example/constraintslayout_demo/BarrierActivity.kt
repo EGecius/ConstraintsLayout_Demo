3e581bad38030de7d055f57b7bce0b297f2b220b
Egidjus Gecius <e.gecius@gmail.com>
2019-08-21 21:21:57 +0100

    add demo for barrier

---
+++ app/src/main/java/com/example/constraintslayout_demo/BarrierActivity.kt
@@ -0,0 +1,12 @@
+package com.example.constraintslayout_demo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class BarrierActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_barrier)
+    }
+}
