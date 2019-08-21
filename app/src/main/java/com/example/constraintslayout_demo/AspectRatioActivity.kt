f6a90877ecd2a92ba3e0753786e5900d8a53bb9e
Egidjus Gecius <e.gecius@gmail.com>
2019-08-21 21:00:59 +0100

    add activity with a fixed aspect ratio

---
+++ app/src/main/java/com/example/constraintslayout_demo/AspectRatioActivity.kt
@@ -0,0 +1,12 @@
+package com.example.constraintslayout_demo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AspectRatioActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_aspect_ratio)
+    }
+}
