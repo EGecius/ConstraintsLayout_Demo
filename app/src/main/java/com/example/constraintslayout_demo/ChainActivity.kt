5acbaa00cfc0b3c627a53a3c8174d384a964d417
Egidjus Gecius <e.gecius@gmail.com>
2019-08-21 20:35:57 +0100

    add packed chain

---
+++ app/src/main/java/com/example/constraintslayout_demo/ChainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.constraintslayout_demo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/** Shows how to user Chains with ConstraintLayout */
+class ChainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chain)
+    }
+}
